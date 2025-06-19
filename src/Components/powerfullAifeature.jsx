91659eb20319418099e6fc245e28699b1f69ac90
Rubul-Tanti <rubultanti404@gmail.com>
2025-06-20 00:58:27 +0530

    changed img name

--- src/Components/powerfullAifeature.jsx
+++ src/Components/powerfullAifeature.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import illustration1 from "../assets/illustration.png"
+import illustration from "../assets/illustration.png"
 import illustration2 from "../assets/2illustration.png"
 import illustration3 from "../assets/3illustration.png"
 import illustration4 from "../assets/4illustration.png"
@@ -9,7 +9,7 @@ const PowerfullAifeature = () => {
 <h2 className='lg:w-1/2 md:4/5 sm:w-4/5 w-4/5 lg:text-5xl md:text-4xl sm:text-2xl max-sm:text-xl  font-bold  text-[#252B37] '>Powerful AI Features for LinkedIn Growth</h2>
   <div className='grid md:grid-col-2 max-sm:grid-1 grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 lg:mt-14 md:mt-10 sm:mt-5 lg:gap-5 md:gap-4 sm:gap3 gap-2 mt-5 p-5 w-[90%] rounded-lg'>
 <div className='p-5 flex flex-col bg-white rounded-lg lg:gap-5 md:gap-4 sm:gap3 gap-2 '>
-<img src={illustration1}/>
+<img src={illustration}/>
 <h4 className='lg:text-2xl md:text-xl max-md:text-2xl font-semibold text-[#252B37]'>AI Content & Image Creator</h4>
 <p className='text-xsm text-[#666D80]'>Generate engaging posts & visuals in seconds, tailored to your company's brand. Use Eldrin's brainstorming features to generate engaging, optimal content for your voice.</p>
 </div>
